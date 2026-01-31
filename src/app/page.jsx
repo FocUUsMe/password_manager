a47d29b67010eb1eada8252d8457b642ae099b9a
FocUUsMe <timoxxastolarov@gmail.com>
2026-01-31 20:32:27 +0300

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,44 @@
   'use client'
 
   import styles from "./page.module.css";
-  import Image from "next/image";
   import data from "@/data/data.json";
+  
+  import Image from "next/image";
   import { useState } from "react";
+  import { useEffect } from "react";
 
-  import NavBar from "@/components/molecules/navBar/navBar";
-
-  import { Roboto_Mono } from 'next/font/google';
-      
-  const font = Roboto_Mono({
-      subsets: ['latin']
-  });
+  import NavItem from "@/components/atoms/navItem/navItem";
 
   export default function Home() {
+    const [items, setItems] = useState([]);
 
+    useEffect(() => {
+      if(!data.cards){
+        throw new Error('File is not defined');
+      }
+
+      console.log('Effect started');
+      
+      setItems( data.cards );
+
+      console.log('Effect ended');
+    }, []);
 
     return (
       <div className={styles.page}>
         <main className={styles.main}>
-          <NavBar />
+          <nav className={styles.nav_bar}>
+            {items.map((item, index) => <NavItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                img_src={item.icon}
+              />
+            )}
+          </nav>
 
-          <div className={styles.info}>
-              <span>
-                
-              </span>
+          <div className={styles.items_info}>
+
           </div>
         </main>
       </div>
---
+++ src/components/atoms/navItem/navItem.jsx
@@ -0,0 +1,27 @@
+    import styles from "@/components/atoms/navItem/styles.module.css";
+    import Image from "next/image";
+
+    import { Chivo_Mono } from "next/font/google";
+
+    const font = Chivo_Mono({
+        subsets: ['latin']
+    });
+    
+    const NavItem = ( props ) => {
+        const {title, img_src} = props;
+        
+        return (
+            <div className={`${styles.nav_item} ${font.className}`}>
+                <Image 
+                    src={img_src}
+                    alt='item_icon'
+                    width={40}
+                    height={40}
+                />
+
+                <span className={styles.item_title}> {title} </span>
+            </div>
+        )             
+    }
+
+    export default NavItem;
